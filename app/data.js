a9db3dfbe4bd5deb4b4d607b2f08c82fb678b08b
kaseifd <mfeijoo2911@esdmadrid.es>
2021-04-23 21:25:03 +0200

    Slider con java y Tippy,js

---
+++ app/data.js
@@ -0,0 +1,72 @@
+const home = {
+    slider: [
+        {
+            img: "assets/img/slider_0.png",
+            card: {
+                appear: false,
+                project: "Space Travel App",
+                author: {
+                    name: "Masud Rana",
+                    link: "https://dribbble.com/mrstudio",
+                    img: "https://cdn.dribbble.com/users/6047818/avatars/normal/84b15dbafef241b1493507776816d4b0.jpg?1600202707",
+                },
+            },
+            text: true,
+        },
+
+        {
+            img: "assets/img/slider_1.png",
+            card: {
+                appear: true,
+                project: "ARS ⚡️ Shopping with your character",
+                author: {
+                    name: "UBANI",
+                    link: "https://dribbble.com/u-ban",
+                    img: "https://cdn.dribbble.com/users/5698127/avatars/normal/ed5059731f2bd3b50ca260dddb57582d.png?1603739415",
+                },
+            },
+            text: false,
+        },
+
+        {
+            img: "assets/img/slider_2.png",
+            card: {
+                appear: true,
+                project: "Dreamer Mode",
+                author: {
+                    name: "Marvin Schwaibold",
+                    link: "https://dribbble.com/MarvinSchwaibold",
+                    img: "https://cdn.dribbble.com/users/969863/avatars/normal/12dcf07b1c9c49a41bbeeabda4907b6a.jpg?1584884798",
+                },
+            },
+            text: false,
+        },
+
+        {
+            img: "assets/img/slider_3.png",
+            card: {
+                appear: true,
+                project: "Smarter",
+                author: {
+                    name: "Deividas Bielskis",
+                    link: "https://dribbble.com/contrast8",
+                    img: "https://cdn.dribbble.com/users/1769954/avatars/normal/f36c405702847471e0f32a32d8feb7e6.png?1550652488",
+                },
+            },
+            text: false,
+        },
+        {
+            img: "assets/img/slider_4.png",
+            card: {
+                appear: true,
+                project: "My Pet",
+                author: {
+                    name: "Nguyen Nhut",
+                    link: "https://dribbble.com/nguyenhut",
+                    img: "https://cdn.dribbble.com/users/721278/avatars/normal/a18e0aaffda22ab6beca8ae4dda36375.jpeg?1594997691",
+                },
+            },
+            text: false,
+        }
+    ]
+}
--- app/main.js
+++ app/main.js
@@ -1,34 +1,125 @@
-const mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    
-    direction: 'horizontal',
-    loop: true,
-    
-    // If we need pagination
-    pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-            
-    },
+window.addEventListener('load', () => {
+        renderSlider();
+        swiper();
+        tooltip();
 
-    //con el teclado
 
-    keyboard: {
-        enabled: true,
-      },
 
-    // Navigation arrows
-    navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-    }     
+
 });
 
 
 
+const swiper = () => {
+        const mySwiper = new Swiper('.swiper-container', {
+                // Optional parameters
 
-tippy('.hasToolTip', {
-        content: 'Click to see project' 
-}); 
+                direction: 'horizontal',
+                loop: true,
+                simulateTouch: false,
+
+                // If we need pagination
+                pagination: {
+                        el: '.swiper-pagination',
+                        clickable: true
+
+                },
+
+                //con el teclado
+
+                keyboard: {
+                        enabled: true,
+                },
+
+                // Navigation arrows
+                navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                }
+        });
+
+}
+
+
+const tooltip = () => {
+
+        tippy('.hasToolTip', {
+                content: 'Click to see project'
+        });
+
+}
+
+
+const renderSlider = () => {
+
+        const sliderWrapper = document.querySelector(".swiper-wrapper")
+        let htmlString = ""
+
+        for (let i = 0; i < home.slider.length; i++) {
+                const slidesHTMLString =
+                        `<div class="swiper-slide">
+                        <img src="${home.slider[i].img}" alt="">
+                        <div class="text">
+                            <h1 class="slider_title">Discover the world’s top designers & creatives</h1>
+                            <p class="slider_description">Dribbble is the leading destination to find & showcase creative work and home to the world's best design professionals.
+                            </p>
+                            <a href="#" class="boton">Sign in</a>
+                        </div>
+                        <div class="slider_card hasToolTip" data-tippy-placement="bottom">               
+                            <div class="name">
+                                <h3 class="name_project">
+                                ${home.slider[i].card.project}
+                                </h3>
+                                <p class="author">by <a href="${home.slider[i].card.author.link}">${home.slider[i].card.author.name}</a></p>
+                            </div>
+                            <img src="${home.slider[i].card.author.img}" alt="" class="author_image">
+                            <!-- <a href="#" class="boton">See project</a>-->
+                        </div>
+                    </div>`;
+                htmlString += slidesHTMLString;
+        }
+
+        sliderWrapper.innerHTML = htmlString
+
+
+
+
+        const texts = document.querySelectorAll(".text")
+        const cards = document.querySelectorAll(".slider_card")
+
+
+        for (let i = 0; i < texts; i++); {
+                for (let a = 0; a < home.slider.length; a++) {
+                        if (home.slider[0].text == true) {
+                                texts[0].classList.add("first_slide")
+                        }
+                }
+        }
+
+
+        for (let b = 0; b < cards; b++); {
+                for (let c = 0; c < home.slider.length; c++) {
+                        if (home.slider[0].card.appear == false) {
+                                cards[0].classList.add("first_slide")
+                        }
+                }
+        }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
